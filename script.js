86d13a299b4ab02ece860d7caf84e50aca1e1110
RSJR-19 <sumbadrenan619@gmail.com>
2025-08-02 15:42:14 +0800

    ui adjusts = no mobile support

---
+++ script.js
@@ -0,0 +1,12 @@
+const mainWrapper = document.getElementById("mainWrapper");
+const frontPage = document.getElementById("frontPage");
+const frontPageH4 = document.getElementById("frontPageH4");
+const frontPageH1 = document.getElementById("frontPageH1");
+function scale(){
+mainWrapper.classList.add("active");
+frontPageH4.classList.add("fade");
+frontPageH1.classList.add("fade");
+
+
+
+}
