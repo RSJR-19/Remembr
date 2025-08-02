c59368fff42a7589b72c01adfbe5b78d7e36dc45
RSJR-19 <sumbadrenan619@gmail.com>
2025-08-02 16:17:28 +0800

    choices screen ui

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const mainWrapper = document.getElementById("mainWrapper");
 const frontPage = document.getElementById("frontPage");
 const frontPageH4 = document.getElementById("frontPageH4");
 const frontPageH1 = document.getElementById("frontPageH1");
+const choicesScreen = document.getElementById("choicesScreen");
+
+choicesScreen.style.display = "none";
+
 function scale(){
 mainWrapper.classList.add("active");
 frontPageH4.classList.add("fade");
 frontPageH1.classList.add("fade");
+setTimeout(()=>{
+frontPage.style.display = "none";
+choicesScreen.style.display = "flex";
+
+setTimeout(() => {
+choicesScreen.classList.add("fadeIn");
+},50);
+},500)
+
 
 
 
